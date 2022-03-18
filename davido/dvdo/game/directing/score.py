49733debba3c8024eedf2f4c63353574a703f1da
Luckysandwich7 <Ryanalvord@hotmail.com>
2022-03-17 20:32:23 -0600

    Initial Upload of documents of code

---
+++ davido/dvdo/game/directing/score.py
@@ -0,0 +1,19 @@
+class Score:
+
+    def __init__(self):
+        self.myScore = 0
+        
+        pass
+
+    def set_score_plus(self):
+        self.myScore += 1
+        
+        pass
+
+    def set_score_minus(self):
+        self.myScore -= 1
+        
+        pass 
+
+    def get_score(self):
+        return self.myScore
