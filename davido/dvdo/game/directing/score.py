7648f8853b762e4852fca9138e23f48e9af9410f
Luckysandwich7 <90877920+Luckysandwich7@users.noreply.github.com>
2022-03-23 20:57:52 -0600

    Added Comments

--- davido/dvdo/game/directing/score.py
+++ davido/dvdo/game/directing/score.py
@@ -1,19 +1,30 @@
 class Score:
 
+    """
+    This class pulls the score and updates it as you collect Gems and Stones.
+    """
+
     def __init__(self):
+        """
+        Begins the score at 0
+        """    
         self.myScore = 0
-        
-        pass
+
 
     def set_score_plus(self):
+        """
+        Adds 1 point to the score
+        """
         self.myScore += 1
-        
-        pass
 
     def set_score_minus(self):
+        """
+        Subtracts 1 point from the score
+        """
         self.myScore -= 1
-        
-        pass 
 
     def get_score(self):
+        """
+        Updates the score and presents the new score
+        """
         return self.myScore
